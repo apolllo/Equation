ce01f3499e5e7ff34d9c7e5b4625021a9bd08ea5
Apolllo <apolllo@github.com>
2016-04-26 12:43:15 +0300

    Updated according to comments

--- src/EQSolver1.java
+++ src/EQSolver1.java
@@ -11,28 +11,27 @@ public class EQSolver1 {
     }
 
     public static void solve(double param1, double param2, double param3) {
-        BigDecimal a = new BigDecimal(param1);
-        BigDecimal b = new BigDecimal(param2);
-        BigDecimal c = new BigDecimal(param3);
+        BigDecimal a = BigDecimal.valueOf(param1);
+        BigDecimal b = BigDecimal.valueOf(param2);
+        BigDecimal c = BigDecimal.valueOf(param3);
 
-        double numerator = (b.pow(2).subtract(new BigDecimal(4).multiply(a).multiply(c))).doubleValue();
+        double numerator = (b.pow(2).subtract(BigDecimal.valueOf(4).multiply(a).multiply(c))).doubleValue();
         if (numerator < 0) {
             System.out.println("Equation has no roots");
-            return;
-        }
-
-        BigDecimal x1 = b.negate().add(
-                new BigDecimal( Math.sqrt(numerator) ))
-                .divide(new BigDecimal(2).multiply(a), 10, BigDecimal.ROUND_HALF_UP);
-        BigDecimal x2 = b.negate().subtract(
-                new BigDecimal( Math.sqrt(numerator) ))
-                .divide(new BigDecimal(2).multiply(a), 10, BigDecimal.ROUND_HALF_UP);
-
-        if (x1.equals(x2)) {
-            System.out.println("Equation has 1 root: " + x1.toPlainString());
         } else {
-            System.out.println("Root1: " + x1.toPlainString());
-            System.out.println("Root2: " + x2.toPlainString());
+            BigDecimal x1 = b.negate().add(
+                    BigDecimal.valueOf(Math.sqrt(numerator)))
+                    .divide(BigDecimal.valueOf(2).multiply(a), 10, BigDecimal.ROUND_HALF_UP);
+            BigDecimal x2 = b.negate().subtract(
+                    BigDecimal.valueOf(Math.sqrt(numerator)))
+                    .divide(BigDecimal.valueOf(2).multiply(a), 10, BigDecimal.ROUND_HALF_UP);
+
+            if (x1.equals(x2)) {
+                System.out.println("Equation has 1 root: " + x1.toPlainString());
+            } else {
+                System.out.println("Root1: " + x1.toPlainString());
+                System.out.println("Root2: " + x2.toPlainString());
+            }
         }
     }
 }
